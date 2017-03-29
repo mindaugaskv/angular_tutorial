815d0491baeb078fdcd6dc23a0ff6ac5627fed2d
MKvederas <mkv@nrd.lt>
2017-03-29 20:53:46 +0300

    Pridėta navigacija

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,45 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LegoPartsComponent } from './components/lego-parts/lego-parts.component';
 import { LegoPartDetailsComponent } from './components/lego-part-details/lego-part-details.component';
 import { DataServiceService } from 'app/services/data-service.service';
 import { InMemoryDataService } from "app/services/in-memory-data.service";
+import { LegoToysComponent } from './components/lego-toys/lego-toys.component';
+import { LegoToyDetailsComponent } from './components/lego-toy-details/lego-toy-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LegoPartsComponent,
-    LegoPartDetailsComponent
+    LegoPartDetailsComponent,
+    LegoToysComponent,
+    LegoToyDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/toys',
+        pathMatch: 'full'
+      },
+      {
+        path: 'toys',
+        component: LegoToysComponent
+      },
+      {
+        path: 'parts',
+        component: LegoPartsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
